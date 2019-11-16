4dc1d9036dcd834c38fb6f90824c2459b0912926
Alexander J Franco <lxfranco1@gmail.com>
2019-11-16 12:19:25 -0500

    README added

---
+++ Analyzer.py
@@ -0,0 +1,8 @@
+import twitter
+import tweepy
+
+api = twitter.Api(consumer_key=,
+                  consumer_secret=,
+                  access_token_key=,
+                  access_token_secret=)
+
---
+++ README.md
@@ -0,0 +1,5 @@
+Tweepy documentation
+http://docs.tweepy.org/en/v3.5.0/api.html 
+
+Python-Twitter documentation
+https://python-twitter.readthedocs.io/en/latest/getting_started.html

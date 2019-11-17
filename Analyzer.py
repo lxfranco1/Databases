32ace2528caa21f43dae77f30dd1523aee15d155
Benji Charles <charles,benji33@gmail.com>
2019-11-16 22:46:06 -0500

    Testing Printing Timeline and fixed bug
    Experimenting with tweepy and twitter api.
    Fixed bug that caused auth error. Problem was that there was an extra space in the key.

--- Analyzer.py
+++ Analyzer.py
@@ -1,5 +1,7 @@
+import os
 import twitter
-import tweepy
+import tweepy as tw
+import pandas as pd
 
 #api = twitter.Api(consumer_key='TEQxvBQ2BVUAt0OnoAMIA0yWh',
 #                  consumer_secret='8YbxHRXnVTzKYPs8Qg4mzd7oD9DFAvbwDfVxdhmljeOlFuSP5j',
@@ -8,11 +10,30 @@ import tweepy
 
 consumer_key='TEQxvBQ2BVUAt0OnoAMIA0yWh'
 consumer_secret='8YbxHRXnVTzKYPs8Qg4mzd7oD9DFAvbwDfVxdhmljeOlFuSP5j'
-access_token='2173222812-UpKnOtwJI2TEd29ubii368C9mzE0KZOYw2qUz4y '
+access_token='2173222812-UpKnOtwJI2TEd29ubii368C9mzE0KZOYw2qUz4y'
 access_token_secret='yGXhsuTrXJ3zJV5P4oizxGc7lq2CV5TlOZTChjR0ApWII'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+api = tw.API(auth, wait_on_rate_limit=True)
 
+'''
+search_words = "#wildfires"
+date_since = "2019-10-10"
+
+# Collect tweets
+tweets = tw.Cursor(api.search,
+              q=search_words,
+              lang="en",
+              since=date_since).items(6)
+
+# Iterate and print tweets
+for tweet in tweets:
+    print(tweet.text)
+'''
+
+#goes to my timeline and prints out the tweets on my timeline
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)

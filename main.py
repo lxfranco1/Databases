f68ddc554ce47165461b40c35ec7d56344b0d5ed
Alexander J Franco <lxfranco1@gmail.com>
2019-11-20 16:26:47 -0500

    HTML files added + File management

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from flask import Flask
-from Analyzer import Analyze
-app = Flask(__name__)
+from flask import Flask, render_template
 
+app = Flask(__name__)
+app.debug=True
 
 @app.route("/")
 def home():
+    return render_template("home.html")
 
-    return "Hello, World!"
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()

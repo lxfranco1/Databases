030cf8bdcc99ac8dbeff2bc7f3854f6ef5038949
ake1998 <51679263+ake1998@users.noreply.github.com>
2019-11-30 21:10:11 -0500

    mysql stuff

---
+++ mysql_test.py
@@ -0,0 +1,18 @@
+import MySQLdb
+
+dbConn = MySQLdb.connect(host='localhost', user='twitter_project', passwd='tw33t!123')
+dbCursor = dbConn.cursor()
+
+#dbCursor.execute('create database twitterinfluence')
+dbCursor.execute('use twitterinfluence')
+
+#Create tables
+# user_table = 'create table user(username char(15) primary key, name char(50))'
+# dbCursor.execute(user_table)
+
+# tweet_table = 'create table tweet(oc_url char(1000) primary key, oc_date DATE, oc_likes int(15), oc_location char(50)'
+# dbCursor.execute(tweet_table)
+#
+# retweet_table = 'create table retweet(re_url char(1000) primary key, re_date DATE, re_likes int(15), re_location char(50)'
+# dbCursor.execute(retweet_table)
+

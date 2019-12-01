035ef05350bb16aabeec448ad248af5b63e266cc
ake1998 <51679263+ake1998@users.noreply.github.com>
2019-11-30 21:12:10 -0500

    Add input tweet template

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 app.debug=True
@@ -12,6 +13,19 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/input", methods=['POST', 'GET'])
+def input():
+    if request.method == 'GET':
+        print("no")
+
+    else:
+        tweet_url = request.form['tweet_url']
+        # get url that the user has entered
+        r = requests.get(tweet_url)
+        print(r.text)
+
+
+    return render_template('input.html')
 
 if __name__ == "__main__":
     app.run()

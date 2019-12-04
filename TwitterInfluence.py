1ba34420f93c01ae89b57021b03d8ff60669016b
ake1998 <51679263+ake1998@users.noreply.github.com>
2019-12-04 10:47:40 -0500

    Update TwitterInfluence.py

--- TwitterInfluence.py
+++ TwitterInfluence.py
@@ -9,53 +9,87 @@ class TwitterInfluence(object):
 
     def insert_user(self, username, name):
         print("insert user")
-        sql = "INSERT INTO user (username, name) VALUES (%s, %s)"
-        vals = (username, name)
+        sql_select = "SELECT * FROM user WHERE username = %s"
+        self.dbCursor.execute(sql_select, str(username))
+        record = self.dbCursor.fetchall()
 
-        self.dbCursor.execute(sql, vals)
-        self.dbConn.commit()
+        if len(record) == 0:
+            sql = "INSERT INTO user (username, name) VALUES (%s, %s)"
+            vals = (username, name)
+
+            self.dbCursor.execute(sql, vals)
+            self.dbConn.commit()
 
     def insert_tweet( self, url, date, likes, location, username, replies):
         print("insert tweet")
-        sql = "INSERT INTO tweet (oc_url, oc_date, oc_likes, oc_location, oc_username, oc_replies) VALUES (%s, %s, %s, %s, %s, %s)"
-        vals = (url, date, likes, location, username, replies)
+        sql_select = "SELECT * FROM tweet WHERE oc_url = %s"
+        self.dbCursor.execute(sql_select, url)
+        record = self.dbCursor.fetchall()
 
-        self.dbCursor.execute(sql, vals)
+        if len(record) == 0:
+            sql = "INSERT INTO tweet (oc_url, oc_date, oc_likes, oc_location,oc_username, oc_replies) " \
+              "VALUES (%s, %s, %s, %s, %s, %s)"
+            vals = (url, date, likes, location, username, replies)
 
-        self.dbConn.commit()
+            self.dbCursor.execute(sql, vals)
+
+            self.dbConn.commit()
 
     def insert_retweet(self, url, date, likes, location, username, replies):
         print("insert retweet")
-        sql = "INSERT INTO retweet (re_url, re_date, re_likes, re_location, re_username, re_replies) VALUES (%s, %s, %s, %s, %s, %s)"
-        vals = (url, date, likes, location, username, replies)
-        self.dbCursor.execute(sql, vals)
+        sql_select = "SELECT * FROM retweet WHERE re_url = %s"
+        self.dbCursor.execute(sql_select, str(url))
+        record = self.dbCursor.fetchall()
 
-        self.dbConn.commit()
+        if len(record) == 0:
+            sql = "INSERT INTO retweet (re_url, re_date, re_likes, re_location, re_username, re_replies) " \
+                  "VALUES (%s, %s, %s, %s, %s, %s)"
+            vals = (url, date, likes, location, username, replies)
+            self.dbCursor.execute(sql, vals)
+
+            self.dbConn.commit()
 
     def insert_isa(self, re_url, oc_url, username):
         print("insert isa")
-        sql = "INSERT INTO isa (isa_oc_url, isa_re_url, isa_username) VALUES (%s, %s, %s)"
-        vals = (oc_url, re_url, username)
+        sql_select = "SELECT * FROM isa WHERE isa_oc_url = %s AND isa_username = %s"
+        vals = (str(oc_url), str(username))
+        self.dbCursor.execute(sql_select, vals)
+        record = self.dbCursor.fetchall()
 
-        self.dbCursor.execute(sql, vals)
+        if len(record) == 0:
+            sql = "INSERT INTO isa (isa_oc_url, isa_re_url, isa_username) VALUES (%s, %s, %s)"
+            vals = (oc_url, re_url, username)
 
-        self.dbConn.commit()
+            self.dbCursor.execute(sql, vals)
+
+            self.dbConn.commit()
 
     def insert_posts(self, username, url):
         print("insert posts")
+        sql_select = "SELECT * FROM posts WHERE posts_oc_url = %s AND posts_username = %s"
+        vals = (str(url), str(username))
+        self.dbCursor.execute(sql_select, vals)
+        record = self.dbCursor.fetchall()
 
-        sql = "INSERT INTO posts (posts_oc_url, posts_username) VALUES (%s, %s)"
-        vals = (url, username)
+        if len(record)==0:
+            sql = "INSERT INTO posts (posts_oc_url, posts_username) VALUES (%s, %s)"
+            vals = (str(url), str(username))
 
-        self.dbCursor.execute(sql, vals)
+            self.dbCursor.execute(sql, vals)
 
         self.dbConn.commit()
 
     def insert_repost(self, url):
         print ("insert repost")
-        sql = "INSERT INTO repost (repost_re_url) VALUES (%s)"
-        val = url
+        sql_select = "SELECT * FROM posts WHERE posts_oc_url = %s AND posts_username = %s"
 
-        self.dbCursor.execute(sql, val)
+        self.dbCursor.execute(sql_select, str(url))
+        record = self.dbCursor.fetchall()
 
-        self.dbConn.commit()
+        if len(record) == 0:
+            sql = "INSERT INTO repost (repost_re_url) VALUES (%s)"
+            val = url
+
+            self.dbCursor.execute(sql, val)
+
+            self.dbConn.commit()
